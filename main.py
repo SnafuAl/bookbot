81756361f3299079eb329360bd86d7745b743f67
SnafuAl <snafual@gmail.com>
2025-07-27 13:40:35 +0100

    refactored to add stats.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from stats import count_words
+
 def get_book_text(input_file):
     with open(input_file) as f:
         book_text = f.read()
     return book_text
-
-def count_words(input_text):
-    word_list = str(input_text).split()
-    word_count = len(word_list)
-    return word_count
 
 def main():
     frankenstein_text = get_book_text("./books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(input_text):
+    word_list = str(input_text).split()
+    word_count = len(word_list)
+    return word_count
