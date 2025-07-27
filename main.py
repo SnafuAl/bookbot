f5769866ed1ba5995f1616680d588316c14e6b20
SnafuAl <snafual@gmail.com>
2025-07-27 13:28:42 +0100

    added count_words

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ def get_book_text(input_file):
         book_text = f.read()
     return book_text
 
+def count_words(input_text):
+    word_list = str(input_text).split()
+    word_count = len(word_list)
+    return word_count
+
 def main():
     frankenstein_text = get_book_text("./books/frankenstein.txt")
-    print(frankenstein_text)
+    frankenstein_count = count_words(frankenstein_text)
+    print(f"{frankenstein_count} words found in the document")
 
 #Call to main
 main()
